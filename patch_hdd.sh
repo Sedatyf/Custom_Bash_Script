46430921fac58737a7d2d1792daa0576b2a8cb48
Sedatyf <gaxel@hotmail.fr>
2020-07-09 11:06:32 +0000

    Delete a > for the df output
    I need to have a fresh output each time rather than stack all df's output

--- patch_hdd.sh
+++ patch_hdd.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #List all the drives on the computer and save the output in df_output
-df >> /home/sedatyf/Documents/df_output
+df > /home/sedatyf/Documents/df_output
 
 #Search for the word HDD in df_output. Then take the first word. Then delete all except the first word
 HDD_PATH=$(grep 'HDD' /home/sedatyf/Documents/df_output | head -n1 | sed -e 's/\s.*$//')
